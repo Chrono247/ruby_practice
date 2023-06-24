e2aaa17e92614de3a60dfadbae12aa8802c9229f
Mitsuhiro DANNO <infochrono247@gmail.com>
2023-06-24 21:26:23 +0000

    1-10課題提出

---
+++ practice_1-10.rb
@@ -0,0 +1,16 @@
+a = 3
+b = 7
+result = a + b
+puts result
+
+array_month = ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"]
+puts array_month[7]
+
+hello = "Hello,"
+name = "Mitsuhiro Danno"
+world = "'s World!"
+puts hello + name + world
+
+omusubi_tech = "tech"
+omusubi_tech.insert(0, "omusubi")
+puts omusubi_tech
