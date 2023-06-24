66f4ed83ad6b6f0eff562226979b1907ecfac944
Mitsuhiro DANNO <infochrono247@gmail.com>
2023-06-24 21:50:36 +0000

    1-10課題提出

--- practice_1-10.rb
+++ practice_1-10.rb
@@ -14,3 +14,19 @@ puts hello + name + world
 omusubi_tech = "tech"
 omusubi_tech.insert(0, "omusubi")
 puts omusubi_tech
+
+calendar_2023 = {
+  "January" => "1月",
+  "February" => "2月",
+  "March" => "3月",
+  "April" => "4月",
+  "May" => "5月",
+  "June" => "6月",
+  "July" => "7月",
+  "August" => "8月",
+  "September" => "9月",
+  "October" => "10月",
+  "November" => "11月",
+  "December" => "12月"
+}
+puts calendar_2023["December"]
